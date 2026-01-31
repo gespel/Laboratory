029596b199f747a8ebd9dd1991e8f3c9cfdfcc72
gespel <heimbrodt@uni-potsdam.de>
2026-01-31 18:37:59 +0100

    removing build

--- build/Benchmarks-b12d07c_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[Boot performance]==] /home/sten/AudioProgramming/Laboratory/build/Benchmarks [==[Boot performance]==]  )
-set_tests_properties( [==[Boot performance]==] PROPERTIES WORKING_DIRECTORY /home/sten/AudioProgramming/Laboratory/build SKIP_RETURN_CODE 4)
-set( Benchmarks_TESTS [==[Boot performance]==])
--- build/Tests-b12d07c_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/sten/AudioProgramming/Laboratory/build/Tests-b12d07c_tests.cmake")
-  include("/home/sten/AudioProgramming/Laboratory/build/Tests-b12d07c_tests.cmake")
-else()
-  add_test(Tests_NOT_BUILT-b12d07c Tests_NOT_BUILT-b12d07c)
-endif()
